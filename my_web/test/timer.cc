d4621247f5afc86d20696c8894f00f589fc34706
LhdDream <626487728@qq.com>
2020-01-19 01:34:08 +0800

    update test

---
+++ my_web/test/timer.cc
@@ -0,0 +1,15 @@
+////
+//// Created by kiosk on 2020/1/18.
+////
+//#include "../util/timer.h"
+//#include <iostream>
+//#include <unistd.h>
+//
+//int main()
+//{
+//    std::cout << Timer::GetSteadyClockMs() << std::endl;
+//
+//    sleep(180);
+//    std::cout << Timer::GetSteadyClockMs() << std::endl;
+//    return  0;
+//}
