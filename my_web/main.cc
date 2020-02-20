452fe98b213f7583654669cdfe2a94282462c917
LhdDream <626487728@qq.com>
2020-02-20 22:30:10 +0800

    update

--- my_web/main.cc
+++ my_web/main.cc
@@ -1,8 +1,8 @@
 #include "include/core/HttpServer.h"
 #include "util/Threads.h"
-int main()
-{
-    signal(SIGPIPE,SIG_IGN);
+
+int main() {
+    signal(SIGPIPE, SIG_IGN);
     Proxy m_proxy;
     m_proxy.Wait();
     return 0;
